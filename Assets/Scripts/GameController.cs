ea0cd53845794440ab7b3a6529fa4dd3b581cafe
Ivan <ivan@kakashi-hatake.ru>
2021-04-07 15:23:35 +0500

    Initialise branch and dynamic UI

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -14,8 +14,16 @@ public class GameController : MonoBehaviour
 
     public Transform addButtonQuestion; // Кнопка спавна кнопок
     public GameObject buttonQuestionPrefab;  // Префаб кнопки вопроса
-    public Transform spawnPointButton; // Позиция спавна кнопки вопроса
     public Transform ParentButtonQuestions; // Родительский объект для кнопки вопроса
+    
+    [Header("Input fields")]
+    public InputField buttonNameField;
+    public InputField questionField;
+    public List<GameObject> answerInputFields = new List<GameObject>();
+    public GameObject answerInputFieldPref;
+    public Transform answerParent;
+    public Transform addAnswerField;
+    public Transform firstAnswerField;
     
     #endregion
 
@@ -37,26 +45,75 @@ public class GameController : MonoBehaviour
     public void AddButtonQuestion(string name)
     {
         // TODO: сделать добаления названия кнопки
-        var button = Instantiate(buttonQuestionPrefab, spawnPointButton.position, Quaternion.identity);
+        var button = Instantiate(buttonQuestionPrefab, addButtonQuestion.position, Quaternion.identity);
         button.transform.SetParent(ParentButtonQuestions);
-        // Фича прикольная, но не нужна здесь
-        if (name == String.Empty)
-            button.GetComponentInChildren<Text>().text = "Вопрос " + questionButtons.Count.ToString();
-        else button.GetComponentInChildren<Text>().text = name;
+        button.GetComponentInChildren<Text>().text = "Вопрос " + questionButtons.Count.ToString();
+        button.name = "QuestionButton" + (questionButtons.Count).ToString();
+        button.GetComponent<Button>().onClick.AddListener(delegate { SwitchCurrentQuestion(button.GetComponent<Button>()); });
         questionButtons.Add(button);
 
         Question question = new Question();
         questions.Add(question);
-
-        Vector3 newPosSpawn = spawnPointButton.position;
-        newPosSpawn.y -= 60f;
-        spawnPointButton.position = newPosSpawn;
+        
         addButtonQuestion.position = new Vector3(addButtonQuestion.position.x, addButtonQuestion.position.y - 60, 0);
+    }
+
+    // Добавление поля для ответа
+    public void AddAnswerInputField()
+    {
+        var inputField = Instantiate(answerInputFieldPref, addAnswerField.position, Quaternion.identity);
+        inputField.transform.SetParent(answerParent);
+        answerInputFields.Add(inputField);
+        addAnswerField.position = new Vector3(addAnswerField.position.x, addAnswerField.position.y - 100f, 0);
     }
 
     // Кнопка сохранения введных полей в объект question
     public void SaveQuestion()
     {
+        questions[currentQuestion].SetQuestion(questionField.text);
+        questions[currentQuestion].SetButtonName(buttonNameField.text);
         
+        for (int i = 0; i < answerInputFields.Count; i++)
+        {
+            questions[currentQuestion].SetAnswer(answerInputFields[i].GetComponent<InputField>().text);
+        }
+
+        //print(questions[0].answers[2]);
+    }
+
+    // Пересоздание необходимого количества полей ответов
+    public void RespawnAnswers(int a)
+    {
+        foreach(GameObject field in answerInputFields) Destroy(field);
+        answerInputFields.Clear();
+        addAnswerField.position = new Vector3(addAnswerField.position.x, firstAnswerField.position.y, 0);
+
+        for (int i = 0; i < a; i++) AddAnswerInputField();
+    }
+
+    // Смена текущего вопроса
+    public void SwitchCurrentQuestion(Button button)
+    {
+        string name = button.name;
+        string nameDigit = "";
+
+        for (int i = 0; i < button.name.Length; i++)
+        {
+            if (char.IsDigit(name[i]))
+            {
+                nameDigit += name[i];
+            }
+        }
+
+        currentQuestion = Convert.ToInt32(nameDigit);
+        
+        buttonNameField.text = questions[currentQuestion].buttonName;
+        questionField.text = questions[currentQuestion].GetQuestion();
+
+        RespawnAnswers(questions[currentQuestion].answers.Count);
+        for (int i = 0; i < answerInputFields.Count; i++) 
+        {
+            answerInputFields[i].GetComponent<InputField>().text = questions[currentQuestion].answers[i];
+        }
     }
 }
--- Assets/Scripts/Question.cs
+++ Assets/Scripts/Question.cs
@@ -9,7 +9,10 @@ public class Question
 
     public void SetQuestion(string a) { question = a; }
     public string GetQuestion() { return question; }
-    public void AddAnswer(string answ) { answers.Add(answ); }
+    public void SetButtonName(string a) { buttonName = a; }
+    public string GetButtonName() { return buttonName; }
+    public void SetAnswer(string answ) { answers.Add(answ); }
+    public void SetAnswers(List<string> a) {}
     public List<string> GetAnswers() { return answers; }
     public void RemoveLastAnswer() { answers.RemoveAt(answers.Count - 1); }
     public void RemoveAnswerAt(int i) { answers.RemoveAt(i); }
